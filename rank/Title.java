f261d59378190928975dfeb5d01f6d8313931d8a
Lemon <OxLemonxO@users.noreply.github.com>
2017-10-14 16:25:03 +0500

    Make sure everything uploads.

---
+++ rank/Title.java
@@ -0,0 +1,64 @@
+package me.totalfreedom.totalfreedommod.rank;
+
+import lombok.Getter;
+import org.bukkit.ChatColor;
+
+public enum Title implements Displayable
+{
+
+    DEVELOPER("a", "Developer", ChatColor.DARK_PURPLE, "Dev"),
+    OWNER("the", "Owner", ChatColor.BLUE, "Owner"),
+    FOUNDER("the", "Founder", ChatColor.RED, "Founder");
+
+    private final String determiner;
+    @Getter
+    private final String name;
+    @Getter
+    private final String tag;
+    @Getter
+    private final String coloredTag;
+    @Getter
+    private final ChatColor color;
+
+    private Title(String determiner, String name, ChatColor color, String tag)
+    {
+        this.determiner = determiner;
+        this.name = name;
+        this.tag = "[" + tag + "]";
+        this.coloredTag = ChatColor.DARK_GRAY + "[" + color + tag + ChatColor.DARK_GRAY + "]" + color;
+        this.color = color;
+    }
+
+    @Override
+    public String getColoredName()
+    {
+        return color + name;
+    }
+
+    @Override
+    public String getColoredLoginMessage()
+    {
+        return determiner + " " + color + ChatColor.ITALIC + name;
+    }
+    
+    @Override
+    public String getName() {
+        return this.name;
+    }
+    
+    @Override
+    public String getTag() {
+        return this.tag;
+    }
+    
+    @Override
+    public String getColoredTag() {
+        return this.coloredTag;
+    }
+    
+    @Override
+    public ChatColor getColor() {
+        return this.color;
+    }
+
+}
